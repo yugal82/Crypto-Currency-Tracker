4bb392113f1f40ecb0c4328c9a953e38cfc4a3b5
Yugal Khanter <yugalkhanter82@gmail.com>
2022-12-23 01:09:59 +0530

    included .env file

--- src/Context/ContextProvider.jsx
+++ src/Context/ContextProvider.jsx
@@ -9,7 +9,7 @@ const ContextProvider = ({ children }) => {
             method: 'GET',
             headers: {
                 "Content-Type": "application/json",
-                'X-API-KEY': '8SXGNzbNnvbo3QnkWXDVYiYQLXGcDl24'
+                'X-API-KEY': process.env.BLOCK_SPAN_API
             }
         }
         const url = 'https://api.blockspan.com/v1/exchanges/collections?chain=eth-main&exchange=opensea&page_size=100';
@@ -23,7 +23,7 @@ const ContextProvider = ({ children }) => {
     //         method: 'GET',
     //         headers: {
     //             "Content-Type": "application/json",
-    //             authorization: 'Bearer Ok-kPy1Xuy04MIozGNgbYUgN1brBgOgIDRGC1xAYZDwdSOBA'
+    //             authorization: `Bearer ${process.env.BLOCK_DAEMON_API}`
     //         }
     //     }
     //     const url = 'https://svc.blockdaemon.com/nft/v1/ethereum/mainnet/collections?sort_by=name&order=asc'
--- src/components/nft-collection/collection details/CollectionDetails.jsx
+++ src/components/nft-collection/collection details/CollectionDetails.jsx
@@ -11,7 +11,7 @@ const CollectionDetails = () => {
       method: 'GET',
       headers: {
         "Content-Type": "application/json",
-        authorization: 'Bearer Ok-kPy1Xuy04MIozGNgbYUgN1brBgOgIDRGC1xAYZDwdSOBA'
+        authorization: `Bearer ${process.env.BLOCK_DAEMON_API}`
       }
     }
     const url = `https://svc.blockdaemon.com/nft/v1/ethereum/mainnet/assets?contract_address=${params.contract_address}`
@@ -20,18 +20,6 @@ const CollectionDetails = () => {
     setCollectionDetail(data);
     console.log(data);
   }
-
-  // const getAssetsForAddress = async () => {
-  //   const options = {
-  //     method: 'GET',
-  //     headers: { accept: 'application/json', 'X-API-KEY': '8SXGNzbNnvbo3QnkWXDVYiYQLXGcDl24' }
-  //   };
-
-  //   const url = `'https://api.blockspan.com/v1/collections/contract/0x41f56b000fffe17943fb4c182c123767af71d005?chain=eth-main`
-  //   const res = await fetch(url, options);
-  //   const data = await res.json();
-  //   console.log(data);
-  // }
 
   useEffect(() => {
     getAssetsForAddress();
